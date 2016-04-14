2820c0418f5a74bc787ef437ddd5b135f9dd34b8
maxboyko <maxboykomax@gmail.com>
2016-04-14 05:13:02 +0000

    polishing the styles

--- client/js/controllers/navController.js
+++ client/js/controllers/navController.js
@@ -1,23 +1,24 @@
-angular.module("MiracleMorning").controller("navController", ['$scope', '$rootScope','Authenticate', '$location', function($scope, $rootScope, Authenticate, $location) {
-   $scope.isLoggedIn = function(){
-          if ($rootScope.user === undefined){
-              return false;
-          }
-          else if ($rootScope.user === true){
-        return true; 
-          }
-          else{
-              return false;
-          }
-        
+angular.module("MiracleMorning").controller("navController", ['$scope', '$rootScope', 'Authenticate', '$location', function($scope, $rootScope, Authenticate, $location) {
+
+    $scope.isLoggedIn = function() {
+        if ($rootScope.user === undefined) {
+            return false;
+        }
+        else if ($rootScope.user === true) {
+
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
-     $scope.logout = function() {
+    $scope.logout = function() {
         Authenticate.logout()
             .then(function() {
                 $location.path('/');
                 console.log(Authenticate.isLoggedIn());
             });
     }
-    
 
 }])
